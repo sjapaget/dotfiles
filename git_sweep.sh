7e5ff7842127af992b165ccb6e7cfc95e3b66cc4
Samuel Paget <sjapaget@gmail.com>
2023-11-06 18:00:21 +0100

    imporived git sweep

---
+++ git_sweep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+branch=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD)
+if [ "$branch" == "master" ] || [ "$branch" == "main" ]; then
+  git branch --merged "$branch" | grep -v "$branch$" | xargs git branch -d
+  git remote prune origin
+else
+  echo "Unsupported branch name: $branch"
+fi
